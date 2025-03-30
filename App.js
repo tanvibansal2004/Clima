ff348cb3b3c0bceb31f378b91c706671b5f31f23
Tanvi Bansal <bansaltanvi2004@gmail.com>
2025-03-31 00:08:18 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import AppNavigation from './navigation/AppNavigation'
+
+export default function App() {
+  return (
+    <AppNavigation />
+  )
+}
---
+++ constants/index.js
@@ -0,0 +1,19 @@
+export const apiKey = 'edd65c22f06b4456b22105920252803'
+
+export const weatherImages = {
+    'Partly cloudy': require('../assets/images/partlyCloudy.png'),
+    'Moderate rain': require('../assets/images/moderateRain.png'),
+    'Patchy rain possible': require('../assets/images/moderateRain.png'),
+    'Sunny': require('../assets/images/sun.png'),
+    'Clear': require("../assets/images/sun.png"),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Light rain': require('../assets/images/moderateRain.png'),
+    'Moderate rain at times': require('../assets/images/moderateRain.png'),
+    'Heavy rain': require('../assets/images/heavyRain.png'),
+    'Heavy rain at times': require('../assets/images/heavyRain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyRain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyRain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyRain.png'),
+    'other': require('../assets/images/moderateRain.png')
+}
